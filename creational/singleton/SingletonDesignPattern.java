cb4f522a30e15e3c61519c1c93adb07007a123ca
adityaksingh <46274813+adityaksingh@users.noreply.github.com>
2018-12-31 14:21:08 +0530

    Update SingletonDesignPattern.java

--- creational/singleton/SingletonDesignPattern.java
+++ creational/singleton/SingletonDesignPattern.java
@@ -1,18 +1,21 @@
 package org.aditya.designpatterns;
 
+import java.io.Serializable;
+
 /**
  * Singleton Design Pattern is commonly used pattern in software development
  * design. It ensures that there will be only one object for this class. It
  * needs to work fine in following scenarios: 1) Multithreading 2) Clonable 3)
- * Serialization 4)
+ * Serialization
  *
  */
-public class SingletonDesignPattern {
+@SuppressWarnings("serial")
+public class SingletonDesignPattern implements Cloneable, Serializable {
 
 	/*
 	 * Instance of SingleDesignPattern class
 	 */
-	private static SingletonDesignPattern singletonDesignPattern;
+	private static volatile SingletonDesignPattern singletonDesignPattern = null;
 
 	/*
 	 * Private Constructor to make sure it is inaccessible from outside
@@ -21,14 +24,42 @@ public class SingletonDesignPattern {
 	}
 
 	/**
-	 * API to get singleton instance of {@link SingletonDesignPattern}
+	 * API to get singleton instance of {@link SingletonDesignPattern}. In this API,
+	 * double null check logic has been applied to support singleton pattern in
+	 * multi threading environment
 	 * 
 	 * @return instance of {@link SingletonDesignPattern}
 	 */
-	public SingletonDesignPattern getInstance() {
+	public static SingletonDesignPattern getInstance() {
 		if (singletonDesignPattern == null) {
-			singletonDesignPattern = new SingletonDesignPattern();
+			synchronized (SingletonDesignPattern.class) {
+				if (singletonDesignPattern == null) {
+					singletonDesignPattern = new SingletonDesignPattern();
+				}
+			}
 		}
 		return singletonDesignPattern;
+	}
+	
+	/**
+	 * Overridden API clone in order to make sure there will only one object for {@link SingletonDesignPattern}
+	 * 
+	 * @throws CloneNotSupportedException whenever this method will be called exception will be thrown
+	 */
+	@Override
+	public Object clone() throws CloneNotSupportedException
+	{
+		throw new CloneNotSupportedException("Clone operation is not supported on Singleton object");
+		
+	}
+
+	/**
+	 * API added to handle the serialization use case for maintaining singleton behavior
+	 *  
+	 * @return object after deserialization is completed
+	 */
+	protected Object readResolve()
+	{
+		return getInstance();
 	}
 }
